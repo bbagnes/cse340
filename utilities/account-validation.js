91ecfe1213e72b4dec1415a4449b2eb4abbfa307
bbagnes <bbagnes@byupathway.edu>
2025-11-24 15:33:24 -0700

    Added account features.

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -2,6 +2,7 @@
 const utilities = require(".")
   const { body, validationResult } = require("express-validator")
   const validate = {}
+  const accountModel = require("../models/account-model");
 
 /*  **********************************
   *  Registration Data Validation Rules
@@ -31,7 +32,13 @@ const utilities = require(".")
       .notEmpty()
       .isEmail()
       .normalizeEmail() // refer to validator.js docs
-      .withMessage("A valid email is required."),
+      .withMessage("A valid email is required.")
+      .custom(async (account_email) => {
+    const emailExists = await accountModel.checkExistingEmail(account_email)
+    if (emailExists){
+      throw new Error("Email exists. Please log in or use different email")
+    }
+    }),
   
       // password is required and must be strong password
       body("account_password")
@@ -48,6 +55,36 @@ const utilities = require(".")
     ]
   };
 
+  /*  **********************************
+  *  Login Data Validation Rules
+  * ********************************* */
+  validate.loginRules = () => {
+    return [
+      // valid email is required and cannot already exist in the DB
+      body("account_email")
+      .trim()
+      .escape()
+      .notEmpty()
+      .isEmail()
+      .normalizeEmail() // refer to validator.js docs
+      .withMessage("email is invalid."),
+  
+      // password is required and must be strong password
+      body("account_password")
+        .trim()
+        .notEmpty()
+        .isStrongPassword({
+          minLength: 12,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1,
+        })
+        .withMessage("Password does not meet requirements."),
+    ]
+  };
+
+
 /* ******************************
  * Check data and return errors or continue to registration
  * ***************************** */
@@ -63,6 +100,26 @@ validate.checkRegData = async (req, res, next) => {
       nav,
       account_firstname,
       account_lastname,
+      account_email,
+    })
+    return
+  }
+  next()
+};
+
+/* ******************************
+ * Check data and return errors or continue login
+ * ***************************** */
+validate.checkLoginData = async (req, res, next) => {
+  const { account_email } = req.body
+  let errors = []
+  errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav()
+    res.render("account/login", {
+      errors,
+      title: "Registration",
+      nav,
       account_email,
     })
     return
